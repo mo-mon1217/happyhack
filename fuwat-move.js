fedd21809fbba43698053bcf140cd1c0dc24040d
mo-mon1217 <34166452+mo-mon1217@users.noreply.github.com>
2021-02-28 21:40:40 +0900

    Add files via upload

--- fuwat-move.js
+++ fuwat-move.js
@@ -1,15 +1,13 @@
-< script type = "text/javascript" >
-    $(document).ready(function () {
-        $('.fuwat').css('visibility', 'hidden');
-        $(window).scroll(function () {
-            var windowHeight = $(window).height(),
-                topWindow = $(window).scrollTop();
-            $('.fuwat').each(function () {
-                var objectPosition = $(this).offset().top;
-                if (topWindow > objectPosition - windowHeight + 200) {
-                    $(this).addClass("fuwatAnime");
-                }
-            });
+$(document).ready(function () {
+    $('.fuwat').css('visibility', 'hidden');
+    $(window).scroll(function () {
+        var windowHeight = $(window).height(),
+            topWindow = $(window).scrollTop();
+        $('.fuwat').each(function () {
+            var objectPosition = $(this).offset().top;
+            if (topWindow > objectPosition - windowHeight + 200) {
+                $(this).addClass("fuwatAnime");
+            }
         });
-    }); <
-/script>
+    });
+});
